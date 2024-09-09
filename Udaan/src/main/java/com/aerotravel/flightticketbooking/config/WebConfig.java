a450f12db5f417af4b16a5e05632ec8abcaa24ac
soundaleatul <soundaleatul@gmail.com>
2024-09-09 06:43:41 +0530

    succesfully commited project

---
+++ Udaan/src/main/java/com/aerotravel/flightticketbooking/config/WebConfig.java
@@ -0,0 +1,22 @@
+package com.aerotravel.flightticketbooking.config;
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+//@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+//    @Autowired
+//    private MessageSource messageSource;
+//
+//    @Override
+//    public Validator getValidator() {
+//        LocalValidatorFactoryBean factory = new LocalValidatorFactoryBean();
+//        factory.setValidationMessageSource(messageSource);
+//        return factory;
+//    }
+
+//    @Bean
+//    public SpringSecurityDialect securityDialect() {
+//        return new SpringSecurityDialect();
+//    }
+}
